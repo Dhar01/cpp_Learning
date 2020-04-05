63eeedb434add4113e741aa44aad90e351ff986e
Loknath Dhar <loknath-dhar@pm.me>
2020-04-05 22:14:18 +0600

    6.4: Global Variables

---
+++ chap_06/constantGlobalVariables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+const int g_x;      // error: constant varialbe must be initialize
+constexpr int g_w;  // same goes for this.
+
+const int g_y{ 1 };
+constexpr int g_z{ 2 };
+
+void doSomething()
+{
+    std::cout << g_y << '\n';
+    std::cout << g_z << '\n';
+}
+
+int main()
+{
+    doSomething();
+
+    std::cout << g_y << '\n';
+    std::cout << g_z << '\n';
+
+    return 0;
+}
---
+++ chap_06/globalVariables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+ 
+// Variables declared outside of a function are global variables
+int g_x {}; // global variable g_x
+ 
+void doSomething()
+{
+    // global variables can be seen and used everywhere in the file
+    g_x = 3;
+    std::cout << g_x << '\n';
+}
+ 
+int main()
+{
+    doSomething();
+    std::cout << g_x << '\n';
+ 
+    // global variables can be seen and used everywhere in the file
+    g_x = 5;
+    std::cout << g_x << '\n';
+ 
+    return 0;
+}
+// g_x goes out of scope here
+
+/* Consider using a 'g_' prefix for global variables to help
+    differentiate them from local variables. */

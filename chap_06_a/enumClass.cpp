2eeacba0888c1f3ea4d88365abce91090bc23d94
Loknath Dhar <loknathdhar66@gmail.com>
2020-04-21 01:29:23 +0600

    enum class

---
+++ chap_06_a/enumClass.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    enum class Color
+    {
+        red,
+        blue
+    };
+
+    enum class Fruit
+    {
+        banana,
+        apple
+    };
+
+    Color color{ Color::red };
+    Fruit fruit{ Fruit::banana };
+
+    if (color == fruit)
+        std::cout << "color and fruit are equal.\n";
+    else
+        std::cout << "color and fruit are not equal.\n";
+    
+    return 0;
+}

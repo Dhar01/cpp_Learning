5f3238942f27edffc256f52199ff8307bcdde5e7
Loknath Dhar <loknath-dhar@pm.me>
2021-01-08 21:39:38 +0600

    update

---
+++ chap_08/inputString02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+		std::cout << "Pick 1 or 2: ";
+		int choice{};
+		std::cin >> choice;
+
+		std::cin.ignore(32767, '\n');
+
+		std::cout << "Now, enter your name: ";
+		std::string name{};
+		std::getline(std::cin, name);
+
+		std::cout << "Hello, " << name << ", you picked " << choice << '\n';
+
+		return 0;
+}
